9b85b1300edc83711b35b0ad8608895faaf095e3
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2016-05-11 23:30:06 +0700

    finish

--- src/JMan.java
+++ src/JMan.java
@@ -8,11 +8,11 @@ public class JMan extends Piece {
 
 	/** Constructor: a new J*Man at position (x, y) on Map m
      with color red if c = 0, green if c = 1, and yellow if c = 2. 
-     * @param x position of x
+	 * @param x position of x
 	 * @param y position of y
 	 * @param c number color
 	 * @param m Map field
-     */
+	 */
 	public JMan(int x, int y, int c, Map m){
 		super(Piece.JMAN, m);
 		super.setX(x);
@@ -25,11 +25,11 @@ public class JMan extends Piece {
 	/** Constructor: a new J*Man at position (x, y) on Map m
      with color c. Precondition: c is one of
      Color.RED, Color.GREEN, and Color.YELLOW.
-     * @param x position of x.
+	 * @param x position of x.
 	 * @param y position of y.
 	 * @param c Color of piece.
 	 * @param m Map field.
-     */
+	 */
 	public JMan(int x, int y, Color c, Map m){
 		super(Piece.JMAN, m);
 		super.setX(x);
@@ -46,22 +46,12 @@ public class JMan extends Piece {
 
 	/** Move J*Man one step based on the value of i:
         0 = up, 1 = down, 2 = left, 3 = right. */
-	public void step(int i){
+	public void step(int x , int y){
+		int newX = getX()+x;
+		int newY = getY()+y;
 		setActed(true);
-		if ( i == 0 && (getMap().isEmpty(getX(), getY()-1) || canEat(getMap().pieceAt(getX(), getY()-1))) ){
-			this.getMap().move(this.getX(), this.getY(), this.getX(), this.getY() - 1);
-
-		} else if ( i == 1 && (getMap().isEmpty(getX(), getY()+1) || canEat(getMap().pieceAt(getX(), getY()+1))) ){
-			this.getMap().move(this.getX(), this.getY(), this.getX(), this.getY() + 1);
-
-		} else if ( i == 2 && (getMap().isEmpty(getX()-1, getY()) || canEat(getMap().pieceAt(getX()-1, getY()))) ){
-			this.getMap().move(this.getX(), this.getY(), this.getX() -1, this.getY());
-
-		} else if ( i == 3 && (getMap().isEmpty(getX()+1, getY()) || canEat(getMap().pieceAt(getX()+1, getY()))) ){
-			this.getMap().move(this.getX(), this.getY(), this.getX() +1, this.getY());
-
-		}
-
+		if ( getMap().isEmpty(newX, newY) || canEat(getMap().pieceAt(newX, newY)))
+			this.getMap().move(getX(), getY(), newX, newY);
 	}
 	/**
 	 * Check next position have a piece of Walker and Pillar and change color of JMan when eat the piece.
@@ -69,18 +59,19 @@ public class JMan extends Piece {
 	 * @return boolean check the next position have the piece that can eat.
 	 */
 	public boolean canEat( Piece p){
-		boolean check = (p != null) && (getColorWord().equals("Green") && p.getColorWord().equals("Red") || getColorWord().equals("Red") && p.getColorWord().equals("Yellow") || getColorWord().equals("Yellow") && p.getColorWord().equals("Green"));
-		if (check){
-			if ( p.getColorWord().equals("Red")) setColor(Color.RED);
-			else if ( p.getColorWord().equals("Green")) setColor(Color.GREEN);
-			else if ( p.getColorWord().equals("Yellow")) setColor(Color.YELLOW);
+		String colorJMan = getColorWord();
+		String colorPiece;
+		boolean check = false;
+		if ( p != null){
+			colorPiece = p.getColorWord();
+			check = (p != null) && ( colorJMan.equals("Green") && colorPiece.equals("Red") || colorJMan.equals("Red") && colorPiece.equals("Yellow") || colorJMan.equals("Yellow") && colorPiece.equals("Green"));
 		}
+		if (check) setColor( p.getColor() );
 		return check;
 	}
 
 	/** = representation of this piece */
 	public String toString() {
-		String color= "";
 		return getColorWord() + " J*Man at (" + getX() + ", " + getY() + ")";
 	}
 }
